c7385b3403909bfde4407d2661ad159e9569794e
York-Andy <141681158+York-Andy@users.noreply.github.com>
2023-08-17 17:55:34 -0300

    Add files via upload
    agregado archivo principal

---
+++ Productos/src/productos/Producto.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package productos;
+
+/**
+ *
+ * @author Matias
+ */
+public class Producto {
+    
+    private String categoria;
+    private String nombre;
+    private double precio;
+
+    public Producto(String categoria, String nombre, double precio) {
+        this.categoria = categoria;
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+    
+    
+}
---
+++ Productos/src/productos/Productos.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package productos;
+
+/**
+ *
+ * @author Matias
+ */
+public class Productos  {
+
+    private String categoria;
+
+    public Productos(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    
+    @Override
+    public String toString() {
+        return categoria;
+    }
+            
+    
+
+}
